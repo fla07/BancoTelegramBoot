6abed161d422bdc6b2c5a7694ed10f1c8329b84b
fla07 <flavitta007@gmail.com>
2019-01-20 17:00:02 -0200

    criacao do Main - FlaviaBank

---
+++ src/FlaviaBank.java
@@ -0,0 +1,27 @@
+
+
+/**
+ * @author Flavia Pino
+ * Projeto final DESENVOLVIMENTO DE APLICAÇÕES JAVA SOA E INTERNET DAS COISAS
+ * FUNDAMENTOS DA TECNOLOGIA JAVA E MODELAGEM VISUAL UML 2.0
+ * Turma 32SCJ
+ *  
+ */
+public class FlaviaBank {
+
+	/**
+	 * 
+	 */
+	public FlaviaBank() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
